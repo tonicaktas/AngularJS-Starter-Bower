fffc217cdc73ab6ae2e04ba4afd113161dceb178
Toni Caktas <tonicaktas@yahoo.com>
2018-01-23 17:43:06 +0100

    Update app.js
    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 angular.module("myApp",['ngRoute','myApp.page1','myApp.page2'])
-.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){ // lägger till route 
   $locationProvider.hashPrefix('!');
   $routeProvider
   .otherwise({
-    redirectTo:"/page1"
+    redirectTo:"/page1" // redirect to startPage om inget annat
   })
 }]);
