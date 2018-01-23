a31fc13857af51c88b5a93f6b54c9a7b14460bca
Toni Caktas <tonicaktas@yahoo.com>
2018-01-23 17:42:41 +0100

    Update page2.js
    update

--- page2/page2.js
+++ page2/page2.js
@@ -1,11 +1,11 @@
 angular.module("myApp.page2",['ngRoute'])
 .config(['$routeProvider',function($routeProvider){
   $routeProvider
-  .when("/page2",{
-    templateUrl:"page2/page2.html",
-    controller:"page2Ctrl"
+  .when("/page2",{ // när denna url så gäller:
+    templateUrl:"page2/page2.html", // visa denna templateUrl
+    controller:"page2Ctrl" // denna controller gäller för denna templateUrl
   })
 }])
-.controller('page2Ctrl',function(){
+.controller('page2Ctrl',function(){ // controller med funktioner
   console.log('page2');
 });
